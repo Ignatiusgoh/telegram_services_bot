b8ee7fd21ddc06d5ead44dbdaea26ec827c2197a
Ignatiusgoh <ignatiusgoh97@gmail.com>
2025-06-26 15:38:58 +0800

    added 2 services

--- config.py
+++ config.py
@@ -3,19 +3,17 @@ SERVICES = {
         "script": "main.py",
         "folder": "/root/Bollinger_band_entry_public",
         "log": "/root/Bollinger_band_entry_public/execution.log"
+    },
+    "Listener": {
+        "script": "main.py",
+        "folder": "/root/binance-event-listener/src",
+        "log": "/root/binance-event-listener/src/binance_event_listener.log"
+    },
+    "Breakeven_adjuster": {
+        "script": "main.py",
+        "folder": " /root/breakeven-adjuster/src",
+        "log": "/root/breakeven-adjuster/src/breakeven_adjuster.log"
     }
-    # ,
-    # "Listener": {
-    #     "script": "main.py",
-    #     "folder": "/path/to/binance-event-listener",
-    #     "log": "/path/to/binance-event-listener/log.txt"
-    # }
-    # ,
-    # "Breakeven_adjuster": {
-    #     "script": "main.py",
-    #     "folder": "/path/to/binance-event-listener",
-    #     "log": "/path/to/binance-event-listener/log.txt"
-    # }
 }
 
 TELEGRAM_TOKEN = "7518373336:AAGIfxUfLQJ40t5rsfEVSbkMqjvgFxpz5tc"
